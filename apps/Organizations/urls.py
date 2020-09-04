50142d25a568674b1dc468a83d127dd52bc1cb65
patel3m <patel3m@uwindsor.ca>
2020-09-03 23:52:33 -0400

    Modified urls

--- apps/Organizations/urls.py
+++ apps/Organizations/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.createOrganization, name='createorganization'),
     path('', views.viewOrganizations, name='vieworganizations'),
-    path('update/<int:id>/', views.updateOrganization, name='updateorganization'),
-    path('delete/<int:id>/', views.deleteOrganization, name='deleteorganization'),
+    path('create/', views.createOrganization, name='createorganization'),
+    path('<int:id>/update/', views.updateOrganization, name='updateorganization'),
+    path('<int:id>/delete/', views.deleteOrganization, name='deleteorganization'),
     path('search/', views.searchOrganization, name='searchorganization'),
     path('search_auto/', views.orgAutoComplete, name='orgautocomplete'),
     path('<int:id>/appointments/', views.viewOrgAppointments, name='vieworgappointments'),
